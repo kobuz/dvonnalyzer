1b2ee54b0f1ed9a6759c494a37b42614d06666aa
Marcin Sokół <kobbuz@gmail.com>
2014-11-04 23:12:05 +0100

    Display game record file content.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
                  [ring/ring-defaults "0.1.2"]
-                 [selmer "0.7.2"]]
+                 [selmer "0.7.2"]
+                 [clj-http-lite "0.2.0"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler dvonnalyzer.core.handler/app}
   :profiles
--- src/dvonnalyzer/core/handler.clj
+++ src/dvonnalyzer/core/handler.clj
@@ -3,12 +3,36 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [selmer.parser :refer [render-file]]
-            [ring.util.anti-forgery :refer [anti-forgery-field]]))
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
+            [ring.util.response :refer [redirect]]
+            [clj-http.lite.client :as client]))
+
+(defn- get-game-id
+  [id-or-url]
+  (re-find #"\d+" id-or-url))
+
+(defn- little-golem-game-url
+  [game-id]
+  (str "http://www.littlegolem.net/servlet/sgf/"
+       game-id "/game" game-id ".txt"))
+
+(defn- parse-game-file
+  [game-file]
+  game-file)
 
 (defroutes app-routes
-  (GET "/" [] (render-file "templates/home.html" {:csrf (anti-forgery-field)}))
-  (POST "/game" [game-id] (str "poscik" game-id))
-  (GET "/game/:game-id" [game-id] (str game-id))
+  (GET "/" [] (render-file "templates/home.html"
+                           {:csrf (anti-forgery-field)}))
+  (POST "/game" [game-id]
+        (let [real-game-id (get-game-id game-id)]
+          (redirect (str "/game/" real-game-id))))
+  (GET "/game/demo" []
+       (let [content (slurp "resources/games/demo.txt")]
+         (parse-game-file content)))
+  (GET "/game/:game-id" [game-id]
+       (let [url (little-golem-game-url game-id)
+             content (:body (client/get url))]
+         (parse-game-file content)))
   (route/not-found "<h1>Not Found</h1>"))
 
 (def app
