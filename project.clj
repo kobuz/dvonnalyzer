317d0b9ac67ef20ab289fa904f1fa0a8d04f0472
Marcin Sokół <kobbuz@gmail.com>
2014-11-04 21:44:36 +0100

    Basic routing and home page template.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [selmer "0.7.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler dvonnalyzer.core.handler/app}
   :profiles
--- src/dvonnalyzer/core/handler.clj
+++ src/dvonnalyzer/core/handler.clj
@@ -1,11 +1,15 @@
 (ns dvonnalyzer.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [selmer.parser :refer [render-file]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" [] (render-file "templates/home.html" {:csrf (anti-forgery-field)}))
+  (POST "/game" [game-id] (str "poscik" game-id))
+  (GET "/game/:game-id" [game-id] (str game-id))
+  (route/not-found "<h1>Not Found</h1>"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
